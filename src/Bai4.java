8f34b3005344ea7334b805a607c8c89173ea1484
sheep <samcoxe0307@gmail.com>
2024-04-03 09:09:26 +0700

    File Bai Tap Buoi 1

---
+++ src/Bai4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập năm cần kiểm tra ");
+        int year = sc.nextInt();
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(" Năm nhuận ok ");
+        } else {
+            System.out.println(" Năm không nhuận ");
+        }
+    }
+}
+//        Scanner ip = new Scanner(System.in);
+//        int year;
+//        System.out.println("Nhập số năm cần kiểm tra");
+//        year = ip.nextInt();
+//        boolean isLeapYear = false;
+//        boolean isBy4 = year % 4 == 0;
+//        if(isBy4) {
+//            boolean isBy100 = year % 100 == 0;
+//            if (isBy100) {
+//                boolean isBy400 = year % 400 == 0;
+//                if (isBy400) {
+//                    isLeapYear = true;
+//                }
+//            }
+//            else {
+//                isLeapYear = true;
+//            }
+//        }
+//        if (isLeapYear) {
+//            System.out.println( year + " là năm nhuận");
+//        }
+//        else {
+//            System.out.println(year + " không phải là năm nhuận ");
+//        }
+//
+//
+//    }
+//}
---
+++ src/Bai9.java
@@ -0,0 +1,6 @@
+public class Bai9 {
+    public static void main(String[] args) {
+
+    }
+
+}
